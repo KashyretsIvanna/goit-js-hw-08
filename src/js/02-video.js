e8e1abdd74db932a47c3b609f57c837017a29bf4
ZadAndRot <kasirecivanna@gmail.com>
2022-08-08 19:24:48 +0300

    feat: :bug: formated

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,11 +3,13 @@ const player = new Vimeo.Player(iframe);
 let t = JSON.parse(localStorage.getItem('videoplayer-current-time'));
 // let _ = require('lodash');
 
-player.on('timeupdate', _.throttle(data => {
-
-  console.log(data);
-  localStorage.setItem('videoplayer-current-time', data.seconds);
-},3000))
+player.on(
+  'timeupdate',
+  _.throttle(data => {
+    console.log(data);
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 3000)
+);
 
 player
   .setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')))
