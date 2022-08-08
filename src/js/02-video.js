4467635375715fe1ea4005d6ae8fe7b6a2c650fe
ZadAndRot <kasirecivanna@gmail.com>
2022-08-08 09:27:23 +0300

    feat: :sparkles: first task has been completed

---
+++ src/js/02-video.js
@@ -0,0 +1,22 @@
+const vid = document.querySelector('iframe');
+console.log(vid);
+// el.contentWindow.location.reload(true);
+
+// setInterval(() => {
+//     const scale = e.offsetX / e.target.getBoundingClientRect().width;
+//     const time = parseInt(video.duration * scale);
+//     const min = parseInt(time / 60);
+//     const sec = time % 60;
+//     return min + ':' + sec;
+
+//   console.log(vid.ariaValueNow);
+// }, 1000);
+
+// setInterval(() => {
+//   console.log(vid.ariaValueMin);
+// }, 1000);
+
+// function func(e) {
+//   console.log(e);
+// }
+
